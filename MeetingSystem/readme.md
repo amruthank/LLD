b4d7988e8d3cacf6e0f3699a37d580d0222156f8
amruthank <amruthank196@gmail.com>
2024-04-13 20:22:07 +0530

    Readme file is updated.

--- MeetingSystem/readme.md
+++ MeetingSystem/readme.md
@@ -1,1 +1,19 @@
-Meeting System
+Meeting System 
+
+    List of functions:
+    2. Add meeting
+    3. Update meeting
+    3. Delete meeting
+    4. If overlap - Dont create a meeting
+    5. Get all events - Date range
+    6. Get an event - event ID
+
+    Events:
+    1. Book an event
+        1.1 Time range [ start time, end time]
+        1.2 Invite meeting guests
+        1.3 Description
+        1.4 Meeting link
+        1.5 Title
+    2. Set reminder - TODO
+    3. Notification - TODO
--- MeetingSystem/src.py
+++ MeetingSystem/src.py
@@ -1,22 +1,3 @@
-"""
-    1. Meeting System - Google calendar System
-    2. Add meeting
-    3. Update meeting
-    3. Delete meeting
-    4. If overlap - Dont create a meeting
-    5. Get all events - Date range
-    6. Get an event - event ID
-
-    1. Book an event
-        1.1 Time range [ start time, end time]
-        1.2 Invite meeting guests
-        1.3 Description
-        1.4 Meeting link
-        1.5 Title
-    2. Set reminder - TODO
-    3. Notification - TODO
-"""
-
 import collections
 from datetime import datetime
 
